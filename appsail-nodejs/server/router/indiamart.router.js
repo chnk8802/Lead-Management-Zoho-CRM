1fd1d03178f7c630c5af65abfb9926c542f0e44d
chnk8802 <chnk8802@gmail.com>
2025-11-27 13:04:12 +0530

    27112025

--- appsail-nodejs/server/router/indiamart.router.js
+++ appsail-nodejs/server/router/indiamart.router.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import { getIndiamartLeadData } from '../controller/indiamart.controller.js';
+import { generateIndiaMartWebhookURL, handleIndiaMartWebhook } from '../controller/indiamart.controller.js';
 import { catalystAuthMiddleware } from '../middleware/auth.middleware.js';
 
 const router = express.Router()
-router.post("/indiamart", catalystAuthMiddleware, getIndiamartLeadData);
+router.get("/generate-webhook-url", catalystAuthMiddleware, generateIndiaMartWebhookURL)
+router.post("/indiamart", handleIndiaMartWebhook);
 
 export default router
---
+++ appsail-nodejs/server/service/crm.servce.js
@@ -0,0 +1,19 @@
+import { crmAxios } from "../utils/crmAxios.utils.js";
+
+export const createLead = async (access_token, payload) => {
+  try {
+    if (!access_token) {
+      throw new Error("Access token is required to create lead");
+    }
+    if (!payload || Object.keys(payload).length === 0) {
+      throw new Error("Payload is required to create lead");
+    }
+    const response = await crmAxios(access_token).post("/Leads", {
+      data: [payload],
+    });
+    return response;
+  } catch (error) {
+    console.error("Error creating lead in Zoho CRM:", error);
+    throw error;
+  }
+};
